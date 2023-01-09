a7be4fbe2c0eee1316f0563902fa93f26a95c4b2
Tamer Zreg <tamer.zreg@stu.oscb.ca>
2023-01-09 19:20:05 +0000

    Added code.py

---
+++ code.py
@@ -0,0 +1,16 @@
+# import ugame
+# import stage
+
+
+def game_scene():
+    # Prints three black lines
+    print("\n\n\n")
+    # Prints Hello World
+    print("Hello, World!")
+    # Place Holder
+    while True:
+        pass
+
+
+if __name__ == "__main__":
+    game_scene()
