8a0a1b88559fb698ca5e9c07cf4382ab9fac2a2c
Tamer Zreg <tamer.zreg@stu.oscb.ca>
2023-01-10 11:21:11 +0000

    Added code.py

--- code.py
+++ code.py
@@ -1,12 +1,34 @@
-# import ugame
-# import stage
+#!/usr/bin/env python3
+
+# Created By: Tamer Zreg
+# Date: Jan. 17th, 2022
+# This Program is a gamed called space aliens invasion
+# where the player fights off an invasion of space aliens.
+
+# Modules used.
+import ugame
+import stage
 
 
 def game_scene():
-    # Prints three black lines
-    print("\n\n\n")
-    # Prints Hello World
-    print("Hello, World!")
+    # Gets image from file (16x16) and sets it as the stage.
+    image_bank_background = stage.Bank.from_bmp16
+    ("space_aliens_background.bmp")
+
+    # Displays image variable image_bank_background 10x8 for each tile.
+    background = stage.Grid(image_bank_background, 10, 8)
+
+    # Displays the image stage background at a rate of 60 Hz and
+    # 60 Frames Per Sec (FPS)
+    game = stage.Stage(ugame.display, 60)
+
+    # Creates a list of layers for the game (In order left appear first)
+    game.layers = [background]
+
+    # Renders all sprites
+    # Mostly renders the background once every game scene.
+    game.render_block()
+
     # Place Holder
     while True:
         pass
